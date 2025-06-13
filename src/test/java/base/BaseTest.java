c17343c7d6b11b35b871279abd0601fdf1af8aaa
Oleg Gavr <gavr0496@gmail.com>
2025-06-14 01:01:03 +0300

    added docker-compose and BaseTest improved

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -53,24 +53,26 @@ public class BaseTest {
     private void initDriver() {
         String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
         Allure.addAttachment("remote", remoteUrl);
-        if (remoteUrl != null || !remoteUrl.isEmpty()) {
-        System.out.println("SELENIUM_REMOTE_URL = " + remoteUrl);
-        //Allure.addAttachment("remote", remoteUrl);
-        if (remoteUrl != null) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");  // Add headless mode
-            options.addArguments("--disable-gpu"); // Switch off GPU, because we don't need it in headless mode
-            options.addArguments("--no-sandbox"); // Switch off sandbox to prevent access rights issues
-            options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
-            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
-            try {
-                driver = new RemoteWebDriver(new URL(remoteUrl), options);
-            } catch (MalformedURLException e) {
-                throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+        if (remoteUrl == null || remoteUrl.isEmpty()) {
+            System.out.println("SELENIUM_REMOTE_URL = " + remoteUrl);
+            //Allure.addAttachment("remote", remoteUrl);
+            if (remoteUrl != null) {
+                Allure.addAttachment("RemoteUrl", remoteUrl);
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");  // Add headless mode
+                options.addArguments("--disable-gpu"); // Switch off GPU, because we don't need it in headless mode
+                options.addArguments("--no-sandbox"); // Switch off sandbox to prevent access rights issues
+                options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
+                options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+                try {
+                    driver = new RemoteWebDriver(new URL(remoteUrl), options);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+                }
+            } else {
+                driver = new ChromeDriver();
             }
-        } else {
-            driver = new ChromeDriver();
         }
-    }
 
+    }
 }
